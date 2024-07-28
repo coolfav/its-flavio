7a489d60d128999cb9b26084a8d294c84cfa2629
coolfav <coolfavo@gmail.com>
2024-07-28 00:32:28 -0400

    moved some files around, added favicon

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import * as THREE from 'three';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey, it's Flavio.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.github.com/coolfav"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         My GitHub
         </a>
       </header>
     </div>
